d6838075dffa5e2ca072cf4dc47099f40dab11b8
Marina294 <69162947+Marina294@users.noreply.github.com>
2020-10-05 13:12:14 -0700

    first commit

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,20 @@
+// For todays assignemnt, create a java program called 'MyFirstJavaProgram'
+// This program should declare a variable with some number as its value. 
+// It should then print it to the console. 
+// then you will reassign the variable to a new value, and print it out again.
+
+
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+
+    int verySmallNum = 100;
+
+    System.out.println(verySmallNum);
+
+    verySmallNum = 5;
+
+    System.out.println(verySmallNum);
+
+    }
+}
+
